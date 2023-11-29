555dd7e6ab0d5571a56dc2a8a0aaf07e47090fc6
katyisonline <katyxmolloy@gmail.com>
2023-11-29 13:42:21 -0500

    Parsed data
    Top 5 youtubers are sorted

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 const chart5 = document.getElementById('chart-5');
 
-var svg = d3.select(chart5)
-    .append('svg')
-    .attr('width', 400)
-    .attr('height', 400)
-  
+
+d3.csv('./data/top_100_youtubers.csv').then(data => {
+
+    const parsedData = [];
+    data.forEach(row => {
+        const channelName = row['ChannelName'];
+        const followerCount = +row['followers'];
+        const q1income = +row['Income q1'];
+        const q2income = +row['Income q2'];
+        const q3income = +row['Income q3'];
+        const q4income = +row['Income q4'];
+        parsedData.push({ channelName, followerCount, q1income, q2income, q3income, q4income })
+    })
+
+
+    parsedData.sort(d3.descending);
+
+    const top5youtubers = parsedData.splice(0, 5)
+    console.log(top5youtubers)
+
+
+    var svg = d3.select(chart5)
+        .append('svg')
+        .attr('width', 400)
+        .attr('height', 400)
+
+    var g = svg
+        .append("g")
+        .attr("transform", "translate(50, 50)")
+        .attr("class", "graph");
+
+})
