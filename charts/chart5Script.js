0b0d7509d06b229290c46af0f7a9ff91812d532b
katyisonline <katyxmolloy@gmail.com>
2023-12-07 18:38:22 -0500

    Added each chart to their respective js files
    Organized my data a bit better, still has to be made responsive

--- charts/chart5Script.js
+++ charts/chart5Script.js
@@ -1,6 +1,91 @@
 const chart5 = document.getElementById('chart-5');
 
-var svg = d3.select(chart5)
-    .append('svg')
-    .attr('width', 400)
-    .attr('height', 400)
+// d3.select(chart5).append('h1').text('Top 5 Youtube Channels Quarterly Income')
+d3.csv('./data/top_100_youtubers.csv').then(data => {
+    var svgwidth = 600;
+    var svgheight = 600;
+    var padding = 100;
+
+    var inner_width = svgwidth - padding;
+    var inner_height = svgheight - padding;
+
+    // GROUPED BAR CHART
+    const top5 = data.splice(0, 5);
+    const top5data = [];
+    top5.forEach(row => {
+        const channelName = row['ChannelName'];
+        const quarters = [{ quarter: 'q1', value: +row['Income q1'] }, { quarter: 'q2', value: +row['Income q2'] },
+        { quarter: 'q3', value: +row['Income q3'] }, { quarter: 'q4', value: +row['Income q4'] }];
+
+        top5data.push({ channelName, quarters })
+    })
+    console.log(top5data)
+    // getting all channel names for x axis
+    var groups = top5data.map((d) => d.channelName)
+ 
+
+
+    var svg = d3.select(chart5)
+        .append('svg')
+        .attr('width', svgheight)
+        .attr('height', svgwidth)
+
+    var g = svg
+        .append("g")
+        .attr("transform", "translate(60, 50)")
+        .attr("class", "graph");
+
+
+    var xscale = d3
+        .scaleBand()
+        .domain(groups)
+        .range([0, inner_width])
+        .padding([0.7]);
+    var xaxis = d3.axisBottom().scale(xscale);
+
+    var yscale = d3
+        .scaleLinear()
+        .domain([0, d3.max(top5data, d => d3.max(d.quarters, q => q.value))])
+        .range([inner_height, 0])
+    var yaxis = d3.axisLeft().scale(yscale);
+
+    g.append("g").
+        call(yaxis);
+
+    g.append("g")
+        .attr("transform", "translate(0, " + inner_height + ")")
+        .call(xaxis);
+
+    var subgroups = ['q1', 'q2', 'q3', 'q4'];
+    var stackedData = d3.stack()
+        .keys(subgroups)(top5data)
+    console.log(stackedData)
+
+
+    var color = d3
+        .scaleOrdinal()
+        .domain(subgroups)
+        .range(["#B21666", "#c95c94", "#7d0f47", "#35021c"]);
+
+    g.append('g')
+        .selectAll('g')
+        .data(stackedData)
+        .join('g')
+        .attr('fill', function (d) { return color(d.key); })
+        .selectAll('rect')
+        .data(function (d) {
+            return d;
+        })
+        .join('rect')
+        .attr('x', function (d) {
+            return xscale(d.channelName)
+        })
+        .attr('y', function (d) {
+            return yscale(d[1]);
+        })
+        .attr('width', xscale.bandwidth())
+        .attr('height', function (d) {
+            return yscale(d[0]) - yscale(d[1])
+        })
+
+})
---
+++ charts/chart7Script.js
@@ -0,0 +1,55 @@
+const chart7 = document.getElementById('chart-7')
+
+// d3.select(chart5).append('h1').text('Top 5 Youtube Channels Quarterly Income')
+d3.csv('./data/top_100_youtubers.csv').then(data => {
+    var svgwidth = 600;
+    var svgheight = 600;
+    var padding = 100;
+
+    var inner_width = svgwidth - padding;
+    var inner_height = svgheight - padding;
+
+    // GROUPED BAR CHART
+    const parsedData = [];
+    data.forEach(row => {
+        const views = +row['Views'];
+        const category = row['Category']
+        parsedData.push({ views, category })
+
+    })
+    parsedData.sort(d3.descending);
+
+    
+    // creating an array of all categories, using the Set object to ensure values are unique
+    const uniqueCategories = [... new Set(parsedData.map(obj => obj.category))]
+
+    // a new array of objects is created by mapping over array of categories to track the category and its total views
+    // views are intialized at 0
+    const viewsByCategory = uniqueCategories.map(category => ({ Category: category, Views: 0 }));
+
+    // iterating over viewsByCategory to add all views 
+    viewsByCategory.forEach((obj) => {
+        // iterates through all data
+        for (let i = 0; i < parsedData.length; i++) {
+            // if the data category is the same as the current category, the views are added to the total
+            if (parsedData[i].category === obj.Category) {
+                obj.Views += parsedData[i].views
+            }
+        }
+    })
+
+    // sorting into descending order
+    viewsByCategory.sort((a, b) => b.Views - a.Views)
+
+    // creating new array of objects for readability, commas are added
+    const mostViews = viewsByCategory.map((obj) => ({
+        Category: obj.Category,
+        Views: obj.Views.toLocaleString()
+    }))
+    console.log(mostViews)
+
+    // data is output in a readable way
+    d3.select(chart7).append('h2').text('Highest Views per Category')
+    d3.select(chart7).append('p').text(`${mostViews[0].Category} has the most views, with ${mostViews[0].Views} total views`)
+
+})
