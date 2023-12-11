0ad67ff4e9d8400722e8eeb8fd344444ecc69def
vasili2142 <vasili2142@gmail.com>
2023-12-11 12:10:11 -0500

    Make text displayed dynamic

--- charts/chart8Script.js
+++ charts/chart8Script.js
@@ -1,9 +1,33 @@
-let h2 = document.createElement('h2');
-h2.textContent = 'Country with highest distribution of top 100 youtubers';
 
-let p = document.createElement('p');
-p.textContent = 'The United States with 40% of the distribution'
 
-let chart8 = document.getElementById('chart-8');
-chart8.appendChild(h2);
-chart8.appendChild(p);
+d3.csv("data/top_100_youtubers.csv").then((data) => {
+
+  var countryCounts = d3.rollup(
+    data, // data being referenced
+    (d) => d.length, // Count occurence of each country, or something else
+    (d) => d.Country // Group data by country
+  );
+
+  var countryData = Array.from(countryCounts, ([country, count]) => ({
+    country,
+    count,
+  }));
+
+  var maxCount = 0;
+  var maxCountCountry = "";
+
+  for (var i = 0; i < countryData.length; i++) {
+    if (countryData[i].count > maxCount) {
+      maxCount = countryData[i].count;
+      maxCountCountry = countryData[i].country;
+    }
+  }  
+
+  let h2 = document.createElement("h2");
+  h2.textContent = 'Country with highest distribution of top 100 youtubers';
+  let p = document.createElement('p');
+  p.textContent = `The ${maxCountCountry} with ${maxCount}%`;
+  let chart8 = document.getElementById("chart-8");
+  chart8.appendChild(h2);
+  chart8.appendChild(p);
+});
